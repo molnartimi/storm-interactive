7cdc98766f090f8d7ac7b43175cf7dddbfe3a050
Molnar Timea <molnar.timi7@gmail.com>
2018-12-01 12:00:05 -0500

    Add philosophers_3 test

--- src/test/java/hu/bme/mit/modelchecker/storm/ProcessRunnerTest.java
+++ src/test/java/hu/bme/mit/modelchecker/storm/ProcessRunnerTest.java
@@ -74,4 +74,45 @@ public class ProcessRunnerTest {
 			e.printStackTrace();
 		}
 	}
+	
+	@Test
+	public void Phil3Test() {
+		ModelParam phil1_eatingRate = new ModelParam("phil1_eatingRate");
+		ModelParam phil2_eatingRate = new ModelParam("phil2_eatingRate");
+		ModelParam phil3_eatingRate = new ModelParam("phil3_eatingRate");
+		ModelReward phil1_thinkingTime = new ModelReward("phil1_thinkingTime");
+		ModelReward phil2_thinkingTime = new ModelReward("phil2_thinkingTime");
+		ModelReward phil3_thinkingTime = new ModelReward("phil3_thinkingTime");
+		ModelReward total_thinkingTime = new ModelReward("Table_totalThinkingTime");
+		InputModel smpl = new InputModelBuilder("/home/storm/git/Stochastic-Optimization/STORM/models/philosophers_3.prism")
+				.withParam(phil1_eatingRate)
+				.withParam(phil2_eatingRate)
+				.withParam(phil3_eatingRate)
+				.withReward(phil1_thinkingTime)
+				.withReward(phil2_thinkingTime)
+				.withReward(phil3_thinkingTime)
+				.withReward(total_thinkingTime)
+				.build();
+		try {
+			AnalysisResult result = new AnalysisBuilder(smpl)
+					.withParam(phil1_eatingRate, 7.33569408796258)
+					.withParam(phil2_eatingRate, 2.156376928966199)
+					.withParam(phil3_eatingRate, 9.680783503298795)
+					.withReward(phil1_thinkingTime)
+					.withReward(phil2_thinkingTime)
+					.withReward(phil3_thinkingTime)
+					.withReward(total_thinkingTime)
+					.withConsolLogInfo(true)
+					.build()
+					.runSteadyStateCheck();
+			
+			assertEquals(result.getResultOf(phil1_thinkingTime), 0.612806126890481, 1e-5);
+			assertEquals(result.getResultOf(phil2_thinkingTime), 0.539690629168788, 1e-5);
+			assertEquals(result.getResultOf(phil3_thinkingTime), 0.971340613831368, 1e-5);
+			assertEquals(result.getResultOf(total_thinkingTime), 2.12383736989064, 1e-5);
+		} catch (IOException | StormException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
